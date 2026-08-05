f503cdecc0ca7fcb232cbfef6280ab6c8cec993c
SantiBueno11 <sbueno937@gmail.com>
2026-08-05 16:05:02 -0300

    configuracion

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' // <- 1. Importar el plugin
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(), // <- 2. Agregarlo a la lista
+  ],
 })
